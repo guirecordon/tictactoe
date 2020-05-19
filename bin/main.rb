1a4c4933831654b60e82a98dabc90948bf1020e8
ioilmio <illuminato.salvatore@gmail.com>
2020-05-20 00:16:52 +0200

    main renamed main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -32,6 +32,8 @@ class TicTacToe
       until @board.valid?(@pick) and @player.unique?(@pick)
         prompt_message(player)
         @pick = gets.chomp.to_i
+        puts 'Choice already picked' if @board.valid?(@pick)
+        puts 'Choose a number between 1 and 9' if @player.unique?(@pick)
       end
       @player.player_array(player) << @pick
       @numbers[@pick - 1] = @player.player_totem(player)
