57e7b4a3ca8b661901491e52b91f8b73149c34db
Gui Recordon <guilherme.recordon@gmail.com>
2020-05-19 15:11:35 -0300

    Move tie and win methods to class Player

--- lib/board.rb
+++ lib/board.rb
@@ -1,17 +1,8 @@
 class Board
-  attr_accessor :player, :numbers
+  attr_accessor :player, :numbers, :arr_player1, :arr_player2
   def initialize
     @numbers = (1..9).to_a
     @player = Player.new
-    @winning_streaks =
-      [[1, 2, 3],
-       [4, 5, 6],
-       [7, 8, 9],
-       [1, 4, 7],
-       [2, 5, 8],
-       [3, 6, 9],
-       [1, 5, 9],
-       [3, 5, 7]]
   end
 
   def display
@@ -28,33 +19,11 @@ class Board
     return true if @numbers.any?(pick) and pick.is_a? Integer
   end
 
-  def win?
-    @winning_streaks.each do |streaks|
-      if @player.player_array(player).include?(streaks) #== streaks
-        puts 'Player 1 wins!'
-        return true
-      elsif (@player.arr_player2two & streaks).to_a.sort == streaks
-        puts 'Player 2 wins!'
-        return true
-      end
-    end
-    false
-  end
-
-  def tie?
-    if @player.arr_player1.size + @player.arr_player2.size == 9 and win? == false
-      puts "It's a tie! Play again."
-      true
-    else
-      false
-    end
-  end
-
   def who_player(i_round)
     if i_round.even?
-     "player2"
+      'player2'
     else
-    "player1"
+      'player1'
     end
   end
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,19 +1,19 @@
 class Player
-  attr_accessor :arr_player1, :arr_player2, :player1, :player2, :board, :pick
+  attr_accessor :arr_player1, :arr_player2, :player1, :player2
   def initialize
-    # @board = Board.new
     @player1 = 'Player 1'
     @player2 = 'Player 2'
     @arr_player2 = []
     @arr_player1 = []
-  end
-
-  def arr_player1one
-    @arr_player1
-  end
-
-  def arr_player2two
-    @arr_player2
+    @winning_streaks =
+      [[1, 2, 3],
+       [4, 5, 6],
+       [7, 8, 9],
+       [1, 4, 7],
+       [2, 5, 8],
+       [3, 6, 9],
+       [1, 5, 9],
+       [3, 5, 7]]
   end
 
   def unique?(pick)
@@ -33,6 +33,28 @@ class Player
       'X'
     else
       'O'
+    end
+  end
+
+  def win?
+    @winning_streaks.each do |streaks|
+      if (@arr_player1 & streaks).to_a.sort == streaks
+        puts 'Player 1 wins!'
+        return true
+      elsif (@arr_player2 & streaks).to_a.sort == streaks
+        puts 'Player 2 wins!'
+        return true
+      end
+    end
+    false
+  end
+
+  def tie?
+    if @arr_player1.size + @arr_player2.size == 9 and win? == false
+      puts "It's a tie! Play again."
+      true
+    else
+      false
     end
   end
 end
